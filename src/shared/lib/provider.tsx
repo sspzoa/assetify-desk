893e01514325013352d24c7130faa3a20df27e0d
Seungpyo Suh <me@sspzoa.io>
2025-11-26 09:17:26 +0900

    💥

---
+++ src/shared/lib/provider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider as JotaiProvider } from "jotai";
+import { type PropsWithChildren, useState } from "react";
+
+export function Providers({ children }: PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+          },
+          mutations: {
+            retry: 1,
+          },
+        },
+      }),
+  );
+
+  return (
+    <JotaiProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </JotaiProvider>
+  );
+}
